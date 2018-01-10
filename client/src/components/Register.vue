a393f2d7238ea10f34483fae16dbda84dd944a6e
Thomas Elder <thomas.charles.elder@gmail.com>
2018-01-10 20:58:17 +1100

    Implemented register page
    A lot happened in the last 5 minutes of that video.
    
    Alright so we've got a register.vue page with fields for taking email
    and password. We bound these to variables using v-model, so we can
    access and amend their values.
    
    We created a register method that uses the authentication service we set
    up previously. This calls the server with the credentials and gets
    a response.
    
    I had to install babel-runtime to avoid errors I was getting, which was
    odd.

--- client/src/components/Register.vue
+++ client/src/components/Register.vue
@@ -5,23 +5,52 @@
     <input
       type="email"
       name="email"
+      v-model="email"
       placeholder="email" />
     <br>
 
     <input
       type="password"
       name="password"
+      v-model="password"
       placeholder="password" />
     <br>
 
-    <button>register</button>
+    <button
+    @click="register">register</button>
   </div>
 </template>
 
 <script>
+  import authentication from '@/services/authentication'
+
   export default {
     data () {
       return {
+        email: ' ',
+        password: ' '
+      }
+    },
+    watch: {
+      email (value) {
+        console.log('email has changed', value)
+      }
+    },
+    mounted () {
+      setTimeout(() => {
+        this.email = 'hello email'
+      }, 2000)
+    },
+    methods: {
+      async register () {
+        console.log('register was clicked', this.email, this.password)
+
+        const response = await authentication.register({
+          email: this.email,
+          password: this.password
+        })
+
+        console.log(response.data)
       }
     }
   }
