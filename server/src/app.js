80d4a910bd7e6cec98bf99129fba258ab64142de
Thomas Elder <thomas.charles.elder@gmail.com>
2018-01-16 22:46:39 +1100

    Set up db
    Started implementing sequelize code to start db and model users.

--- server/src/app.js
+++ server/src/app.js
@@ -4,7 +4,7 @@ const express = require('express')
 const bodyParser = require('body-parser')
 const cors = require('cors')
 const morgan = require('morgan')
-const sequalize = require('./models')
+const {sequelize} = require('./models')
 const config = require('./config/config')
 
 const app = express()
@@ -12,23 +12,10 @@ app.use(morgan('combined'))
 app.use(bodyParser.json())
 app.use(cors())
 
-app.get('/status', (req, res) => {
-  res.send({
-    message: 'hello world'
-  })
-})
+require('./routes')(app)
 
-app.post('/register', (req, res) => {
-  res.send({
-    message: `Hello ${req.body.email}! Your user was registered have fun`
-  })
-})
-
-sequalize.sync()
+sequelize.sync()
   .then(() => {
-
     app.listen(config.port)
-    console.log(`Server st`)
+    console.log(`Server started listening on port ${config.port}`)
   })
-
-app.listen(process.env.PORT || 8081)
